53dbe65f033da97dab98800e1eb2db3372e79769
Cleiton Braga <cleitonbraga56@gmail.com>
2021-04-30 22:45:04 -0300

    Add initial files and components

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './components/home/home.component';
+import { RoomComponent } from './components/room/room.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: ':roomId', component: RoomComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
