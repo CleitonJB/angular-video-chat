dcc83d75aeb0004b8a1f7fab436cb994d005e1c1
Cleiton Braga <cleitonbraga56@gmail.com>
2021-05-03 14:04:22 -0300

    Add angular material And Can access routes in server

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
+import { MatIconModule } from '@angular/material/icon';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -21,11 +25,16 @@ import { SocketIoModule } from "ngx-socket-io";
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+
     SocketIoModule.forRoot({
       url: 'http://localhost:3000'
-    })
+    }),
+
+    MatIconModule
   ],
-  providers: [],
+  providers: [
+    {provide: LocationStrategy, useClass: HashLocationStrategy}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
